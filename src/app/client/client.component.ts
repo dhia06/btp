b34ab66d6b786f67619962a9f95076d5929fbdd3
mohameddhiaelhak.sassi <mohammeddhiaelhak.sassi@esprit.tn>
2021-03-30 11:02:25 +0200

    brp

---
+++ src/app/client/client.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.scss']
+})
+export class ClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+    
+  }
+
+}
